80e91305c5cbe1482533665e42122921b116b0eb
Charles Chiakwa <cchiakwa@seamfix.com>
2020-01-06 18:41:31 +0100

    blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const BlogPage = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Blog Page </h1>
+        <p>This is the blog page, it will show the blog posts</p>
+      </div>
+    </Layout>
+  )
+}
+
+export default BlogPage
