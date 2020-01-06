8da50702ada86ad836e1ae9d94f567bd5fcb6432
Charles Chiakwa <cchiakwa@seamfix.com>
2020-01-06 18:40:01 +0100

    footer component

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>This is the footer layout, copyright 2020</div>
+    </footer>
+  )
+}
+
+export default Footer
