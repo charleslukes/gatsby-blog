be1ea137792fb8494ed7d21be6ac545b53ee25e4
Charles Chiakwa <cchiakwa@seamfix.com>
2020-01-06 18:41:45 +0100

    contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Contact Page </h1>
+        <p>
+          I am very much avaliable on twitter{" "}
+          <a href="https://twitter.com/charles_lukes">@charles_lukes</a>
+        </p>
+      </div>
+    </Layout>
+  )
+}
+
+export default ContactPage
