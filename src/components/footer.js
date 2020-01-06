dbb02a9bc529124c126f25d7eff3bc2383d4bdc9
Charles Chiakwa <cchiakwa@seamfix.com>
2020-01-06 19:09:05 +0100

    add dymanic data to fecth author name in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,19 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `)
   return (
     <footer>
-      <div>This is the footer layout, copyright 2020</div>
+      <div>Created by {data.site.siteMetadata.author} , copyright 2020</div>
     </footer>
   )
 }
